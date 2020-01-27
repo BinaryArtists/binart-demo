4b499d1b59305a8f02e067bdaed97d20dcbfdb23
fallen.ink <fengzilijie@qq.com>
2020-01-27 18:01:15 +0800

    新增 group （集团/公司/部门/岗位等）初始化工程

---
+++ binart-group/src/main/java/com/binart/binartgroup/BinartGroupApplication.java
@@ -0,0 +1,13 @@
+package com.binart.binartgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinartGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinartGroupApplication.class, args);
+    }
+
+}
---
+++ binart-group/src/test/java/com/binart/binartgroup/BinartGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binart.binartgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinartGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
