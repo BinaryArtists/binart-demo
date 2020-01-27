d84e7d0f7e6bc84a56745c4d87db5f4aaa6a2e0c
fallen.ink <fengzilijie@qq.com>
2020-01-27 18:10:24 +0800

    新增 system 模块初始化

---
+++ binart-system/src/main/java/com/binart/binartsystem/BinartSystemApplication.java
@@ -0,0 +1,13 @@
+package com.binart.binartsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinartSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinartSystemApplication.class, args);
+    }
+
+}
---
+++ binart-system/src/test/java/com/binart/binartsystem/BinartSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binart.binartsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinartSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
