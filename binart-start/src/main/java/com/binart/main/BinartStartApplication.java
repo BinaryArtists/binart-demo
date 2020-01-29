4da81c835e77acf8bd0e6ead4a093ff9b81bb6ff
fallen.ink <fengzilijie@qq.com>
2020-01-29 13:43:15 +0800

    修改main为start

---
+++ binart-start/src/main/java/com/binart/main/BinartStartApplication.java
@@ -0,0 +1,13 @@
+package com.binart.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinartStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinartStartApplication.class, args);
+    }
+
+}
