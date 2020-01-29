966c928510b5957131c3873bd8feb35d37a1e786
fallen.ink <fengzilijie@qq.com>
2020-01-29 13:48:57 +0800

    新增积分系统

---
+++ binart-points/src/main/java/com/binart/binartpoints/BinartPointsApplication.java
@@ -0,0 +1,13 @@
+package com.binart.binartpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinartPointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinartPointsApplication.class, args);
+    }
+
+}
