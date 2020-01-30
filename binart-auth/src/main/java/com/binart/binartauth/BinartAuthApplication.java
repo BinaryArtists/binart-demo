9ce85c4bec289a3ca2d7b216ba366fcc813b0d7f
fallen.ink <fengzilijie@qq.com>
2020-01-30 11:55:25 +0800

    鉴权模块 初始化

---
+++ binart-auth/src/main/java/com/binart/binartauth/BinartAuthApplication.java
@@ -0,0 +1,13 @@
+package com.binart.binartauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinartAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinartAuthApplication.class, args);
+    }
+
+}
---
+++ binart-auth/src/test/java/com/binart/binartauth/BinartAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binart.binartauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinartAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
